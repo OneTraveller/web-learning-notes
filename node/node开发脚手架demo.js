03a0fa324c0580c3baaf90590b591770a93e66a0
OneTraveller <15662173002@163.com>
2020-04-06 17:59:14 +0800

    修改node开发脚手架demo

--- node/node开发脚手架demo.js
+++ node/node开发脚手架demo.js
@@ -1,5 +1,13 @@
 // 参考链接： https://github.com/lin-xin/blog/issues/27
 
+/* 
+  前置工作
+  1.建一个新的文件夹
+  2.进入文件夹，执行命令行：npm init -y
+  3.安装相关依赖：npm i -S commander download-git-repo inquirer ora chalk log-symbols
+  4.初始化项目：node index.js init projectName
+*/
+
 // 读取文件
 const fs = require('fs');
 // commander 可以自动的解析命令和参数，用于处理用户输入的命令
@@ -8,8 +16,6 @@ const program = require('commander');
 const download = require('download-git-repo');
 // 使用 inquirer 接收用户的输入并作出相应的处理
 const inquirer = require('inquirer');
-// 使用 handlebars 渲染完后的模板重新写入到文件中
-const handlebars = require('handlebars');
 // 视觉美化相关
 // 使用 ora 来提示用户正在下载中。
 const ora = require('ora');
@@ -27,12 +33,12 @@ program
       inquirer
         .prompt([
           {
-            name: 'description',
-            message: '请输入项目描述'
-          },
-          {
             name: 'author',
             message: '请输入作者名称'
+          },
+          {
+            name: 'description',
+            message: '请输入项目描述'
           }
         ])
         .then(answers => {
@@ -49,19 +55,18 @@ program
                 console.log(symbols.error, chalk.red(err));
               } else {
                 spinner.succeed();
+                // 修改package.json文件，暂时没有找到更好的方法...
                 // 读取package.json文件
                 const fileName = `${name}/package.json`;
-                const meta = {
-                  name,
-                  description: answers.description,
-                  author: answers.author
-                };
-                // 写入 package.json 文件
-                if (fs.existsSync(fileName)) {
-                  const content = fs.readFileSync(fileName).toString();
-                  const result = handlebars.compile(content)(meta);
-                  fs.writeFileSync(fileName, result);
-                }
+                const content = JSON.parse(fs.readFileSync(fileName));
+                content.name = name;
+                content.author = answers.author;
+                content.description = answers.description;
+                const afterContent = JSON.stringify(content, null, 2);
+                // 修改package.json文件
+                fs.writeFile(fileName, afterContent, function(err) {
+                  if (err) console.error(err);
+                });
                 console.log(symbols.success, chalk.green('项目下载完成'));
               }
             }
